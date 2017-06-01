91ba1146c5a81f0ff34bde0f12b5e0389c56bde9
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-06-01 19:00:00 +0100

    feat: timeline event component with event object as input

--- src/app/components/timeline-event/timeline-event.component.ts
+++ src/app/components/timeline-event/timeline-event.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import 'rxjs/add/operator/first';
 
 @Component({
   selector: 'sm-timeline-event',
   templateUrl: 'timeline-event.component.html'
 })
-export class TimelineEventComponent {
+export class TimelineEventComponent implements OnInit {
+
+  @Input() timelineEvent: any;
+  @Input() bgColor: string;
+
+  ngOnInit() {
+
+  }
 
 }
