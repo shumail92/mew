9a2f24092d5778317d302e5782ddebdbf8ec26aa
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-06-24 01:20:38 +0200

    feat(timeline): get events from service

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import 'rxjs/add/operator/first';
+import { TimelineEventsService } from '../../services/events.service'
+import { Event } from '../../model/event';
 
 @Component({
   selector: 'sm-timeline',
-  templateUrl: 'timeline.component.html'
+  templateUrl: 'timeline.component.html',
+  providers: [TimelineEventsService]
 })
 export class TimelineComponent {
+
+  events: Event[];
+
+  constructor(private eventsService: TimelineEventsService) {
+    this.events = this.eventsService.getEvents();
+  }
 
 }
